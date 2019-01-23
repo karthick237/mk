6dc26abafed0fa6db27619e7aac0a7ec9f6021b2
karthick237 <46838602+karthick237@users.noreply.github.com>
2019-01-23 15:55:05 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+char a[100],b[100];
+int i,j,c=0,k=0;
+scanf("%s",&a[i]);
+c=0;
+for(i=0;a[i]!='\0';i++)
+{
+c++;
+}
+    //printf("%d",c);
+    k=0;
+    for(j=c-1;j>=0;j--)
+{
+b[k]=a[j];
+
+    printf("%c",b[k]);
+    k++;
+}
+    return 0;
+}
