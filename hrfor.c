2e9313712d718aa20cf9fd3227aab8a46f6ca53f
karthick237 <46838602+karthick237@users.noreply.github.com>
2019-01-30 16:01:42 +0530

    Create hrfor.c

---
+++ hrfor.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main() 
+{
+    int a, b;
+    scanf("%d\n%d", &a, &b);
+
+  if (a==1) {
+    printf("one\n");
+
+  } else if (a == 2) {
+    printf("two\n");
+  } else if (a == 3) {
+    printf("three\n");
+  } else if (a == 4) {
+    printf("four\n");
+
+  } else if (a == 5) {
+    printf("five\n");
+  } else if (a == 6) {
+    printf("six\n");
+  } else if (a == 7) {
+    printf("seven\n");
+  } else if (a == 8) {
+    printf("eight\n");
+  } else if (a >=9) {
+    printf("nine\n");
+  }
+  if (b == 1) {
+    printf("one");
+
+  } else if (b == 2) {
+    printf("two");
+  } else if (b == 3) {
+    printf("three");
+  } else if (b == 4) {
+    printf("four");
+
+  } else if (b == 5) {
+    printf("five");
+  } else if (b == 6) {
+    printf("six");
+  } else if (b == 7) {
+    printf("seven");
+  } else if (b == 8) {
+    printf("eight");
+  } else if (b>= 9) {
+    printf("nine");
+  } 
+
+  return 0;
+}
