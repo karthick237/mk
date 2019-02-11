09eb39369e6da27f26bb5cdbbc36a3d985f61c4f
karthick237 <46838602+karthick237@users.noreply.github.com>
2019-02-11 13:50:31 +0530

    Create 12.c

---
+++ 12.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<math.h>
+int main() {
+	int n,rem,t,sum=0;
+	scanf("%d",&n);
+	t=n;
+while(n!=0)
+{
+	rem=n%10;
+	sum=sum*10+rem;
+	n=n/10;
+}
+if(t==sum)
+{
+	printf("palindrome");
+}
+else
+printf("not palindrome");
+	return 0;
+}
