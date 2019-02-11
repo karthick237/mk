1f7981bcabcf0aef1f3399a0a65d82fe943cf955
karthick237 <46838602+karthick237@users.noreply.github.com>
2019-02-11 15:25:23 +0530

    Create 42.c

---
+++ 42.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[100],b[100];
+scanf("%s %s",a,b);
+int c,d;
+c=strlen(a);
+d=strlen(b);
+if(c==d)
+{
+printf("%s",a);
+}
+else if(c>d)
+{
+printf("%s",a);
+}
+else
+{
+printf("%s",b);
+}
+return 0;
+}
