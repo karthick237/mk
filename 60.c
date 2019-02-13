d9770580485ceebe97082a70fcf7d40f08e6d78f
karthick237 <46838602+karthick237@users.noreply.github.com>
2019-02-13 22:25:31 +0530

    Update 60.c

--- 60.c
+++ 60.c
@@ -7,8 +7,8 @@ int main()
     printf("%d  %d  ",a,b);
     for(i=2;i<n;i++)
     {
-        c=a+b;
-        printf("%d  ",c);
+       c=a+b;
+       printf("%d  ",c);
         a=b;
         b=c;
     }
