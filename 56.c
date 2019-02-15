f955695eee676a80133c5cf3273ba1abf9033c5f
karthick237 <46838602+karthick237@users.noreply.github.com>
2019-02-15 12:12:48 +0530

    Update 56.c

--- 56.c
+++ 56.c
@@ -3,7 +3,7 @@
 int main() 
 {
 char a[100];
-int i,n,count=0,countt=0,count1=0;
+int i,n,count=0,countt=0;
 scanf("%s",a);
 n=strlen(a);
 	for(i=0;i<n;i++)
@@ -15,10 +15,6 @@ n=strlen(a);
 	    else if((a[i]>=0)&&(a[i]>=9))
 	    {
 	        countt=1;
-	    }
-	    else
-	    {
-	        count1=1;
 	    }
 	}
 	if((count==1)&&(countt==1))
